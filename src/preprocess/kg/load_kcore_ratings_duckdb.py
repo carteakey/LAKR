d856873cfb390176d9d7cfa372843b7485c29c4c
Kartikey Chauhan <kartikeychauhan27@gmail.com>
2024-07-30 03:50:38 -0400

    keep positive ratings only

--- src/preprocess/kg/load_kcore_ratings_duckdb.py
+++ src/preprocess/kg/load_kcore_ratings_duckdb.py
@@ -13,3 +13,11 @@ con.sql("CREATE TABLE rating_only (user_id VARCHAR, item_id VARCHAR, rating FLOA
 con.execute("COPY rating_only FROM '{}' (FORMAT CSV, HEADER FALSE)".format(DATASET))
 
 print(con.execute("SELECT * FROM rating_only LIMIT 5").fetchdf())
+
+# Keep positive ratings only
+
+con.sql("DROP TABLE IF EXISTS rating_only_positive")
+
+con.sql("CREATE TABLE rating_only_positive AS SELECT * FROM rating_only WHERE rating > 3")
+
+print(con.execute("SELECT * FROM rating_only_positive LIMIT 5").fetchdf())
