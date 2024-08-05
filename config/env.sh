f6121841f0dbdcfacae53e17cf4c1cbcda6881d8
Kartikey Chauhan <kartikeychauhan27@gmail.com>
2024-08-05 13:22:10 -0400

    Refactor env.sh to update DUCKDB_PATH variable

--- config/env.sh
+++ config/env.sh
@@ -1,4 +1,4 @@
-DB_FILE=/home/kchauhan/repos/mds-tmu-mrp/db/duckdb/amazon_reviews.duckdb
+DUCKDB_PATH=/home/kchauhan/repos/mds-tmu-mrp/db/duckdb/amazon_reviews.duckdb
 NEO4J_URI=bolt://localhost:7687
 NEO4J_USER=neo4j
 NEO4J_USERNAME=neo4j
